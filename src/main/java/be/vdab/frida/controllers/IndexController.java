41108f9f878fbf72fa880bee5bdff33d1cb33834
hansdewitte <hans.dewitte@vdabcampus.be>
2019-03-07 14:06:27 +0100

    bezocht

--- src/main/java/be/vdab/frida/controllers/IndexController.java
+++ src/main/java/be/vdab/frida/controllers/IndexController.java
@@ -4,10 +4,13 @@ import be.vdab.frida.domain.Adres;
 import be.vdab.frida.domain.Gemeente;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
@@ -18,13 +21,24 @@ import static java.time.DayOfWeek.THURSDAY;
 @RequestMapping("/")
 class IndexController {
     @GetMapping
-    ModelAndView index() {
+    ModelAndView index(HttpServletResponse response,
+                       @CookieValue(name = "terugkerendBezoeker", required = false) String cookie) {
         var modelAndView = new ModelAndView("index");
-        var gemeente = new Gemeente("Akker", 7000);
-        var adres = new Adres("Boerenveldlaan", "3B", gemeente);
-        modelAndView.addObject("adres", adres)
+        modelAndView.addObject("adres", getAdres())
                     .addObject("openOfGesloten", getOpenheidOpBasisVanDag());
+        if (cookie == null) {
+            var newCookie = new Cookie("terugkerendBezoeker", "true");
+            newCookie.setMaxAge(31_536_000);
+            response.addCookie(newCookie);
+        } else {
+            modelAndView.addObject("terugkerendBezoeker", true);
+        }
         return modelAndView;
+    }
+
+    private Adres getAdres() {
+        var gemeente = new Gemeente("Akker", 7000);
+        return new Adres("Boerenveldlaan", "3B", gemeente);
     }
 
     private String getOpenheidOpBasisVanDag() {
